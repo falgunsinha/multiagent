bd171460ba8f2569703c48f58cfdb93fd115a9ea
Falgun Sinha <131682466+falgunsinha@users.noreply.github.com>
2026-02-06 16:23:37 +0100

    --

--- scripts/Reinforcement Learning/on-policy/onpolicy/algorithms/r_mappo/algorithm/r_actor_critic.py
+++ scripts/Reinforcement Learning/on-policy/onpolicy/algorithms/r_mappo/algorithm/r_actor_critic.py
@@ -10,13 +10,6 @@ from onpolicy.utils.util import get_shape_from_obs_space
 
 
 class R_Actor(nn.Module):
-    """
-    Actor network class for MAPPO. Outputs actions given observations.
-    :param args: (argparse.Namespace) arguments containing relevant model information.
-    :param obs_space: (gym.Space) observation space.
-    :param action_space: (gym.Space) action space.
-    :param device: (torch.device) specifies the device to run on (cpu/gpu).
-    """
     def __init__(self, args, obs_space, action_space, device=torch.device("cpu")):
         super(R_Actor, self).__init__()
         self.hidden_size = args.hidden_size
@@ -42,19 +35,6 @@ class R_Actor(nn.Module):
         self.algo = args.algorithm_name
 
     def forward(self, obs, rnn_states, masks, available_actions=None, deterministic=False):
-        """
-        Compute actions from the given inputs.
-        :param obs: (np.ndarray / torch.Tensor) observation inputs into network.
-        :param rnn_states: (np.ndarray / torch.Tensor) if RNN network, hidden states for RNN.
-        :param masks: (np.ndarray / torch.Tensor) mask tensor denoting if hidden states should be reinitialized to zeros.
-        :param available_actions: (np.ndarray / torch.Tensor) denotes which actions are available to agent
-                                                              (if None, all actions available)
-        :param deterministic: (bool) whether to sample from action distribution or return the mode.
-
-        :return actions: (torch.Tensor) actions to take.
-        :return action_log_probs: (torch.Tensor) log probabilities of taken actions.
-        :return rnn_states: (torch.Tensor) updated RNN hidden states.
-        """
         obs = check(obs).to(**self.tpdv)
         rnn_states = check(rnn_states).to(**self.tpdv)
         masks = check(masks).to(**self.tpdv)
@@ -71,19 +51,6 @@ class R_Actor(nn.Module):
         return actions, action_log_probs, rnn_states
 
     def evaluate_actions(self, obs, rnn_states, action, masks, available_actions=None, active_masks=None):
-        """
-        Compute log probability and entropy of given actions.
-        :param obs: (torch.Tensor) observation inputs into network.
-        :param action: (torch.Tensor) actions whose entropy and log probability to evaluate.
-        :param rnn_states: (torch.Tensor) if RNN network, hidden states for RNN.
-        :param masks: (torch.Tensor) mask tensor denoting if hidden states should be reinitialized to zeros.
-        :param available_actions: (torch.Tensor) denotes which actions are available to agent
-                                                              (if None, all actions available)
-        :param active_masks: (torch.Tensor) denotes whether an agent is active or dead.
-
-        :return action_log_probs: (torch.Tensor) log probabilities of the input actions.
-        :return dist_entropy: (torch.Tensor) action distribution entropy for the given inputs.
-        """
         obs = check(obs).to(**self.tpdv)
         rnn_states = check(rnn_states).to(**self.tpdv)
         action = check(action).to(**self.tpdv)
@@ -118,13 +85,6 @@ class R_Actor(nn.Module):
 
 
 class R_Critic(nn.Module):
-    """
-    Critic network class for MAPPO. Outputs value function predictions given centralized input (MAPPO) or
-                            local observations (IPPO).
-    :param args: (argparse.Namespace) arguments containing relevant model information.
-    :param cent_obs_space: (gym.Space) (centralized) observation space.
-    :param device: (torch.device) specifies the device to run on (cpu/gpu).
-    """
     def __init__(self, args, cent_obs_space, device=torch.device("cpu")):
         super(R_Critic, self).__init__()
         self.hidden_size = args.hidden_size
@@ -154,15 +114,6 @@ class R_Critic(nn.Module):
         self.to(device)
 
     def forward(self, cent_obs, rnn_states, masks):
-        """
-        Compute actions from the given inputs.
-        :param cent_obs: (np.ndarray / torch.Tensor) observation inputs into network.
-        :param rnn_states: (np.ndarray / torch.Tensor) if RNN network, hidden states for RNN.
-        :param masks: (np.ndarray / torch.Tensor) mask tensor denoting if RNN states should be reinitialized to zeros.
-
-        :return values: (torch.Tensor) value function predictions.
-        :return rnn_states: (torch.Tensor) updated RNN hidden states.
-        """
         cent_obs = check(cent_obs).to(**self.tpdv)
         rnn_states = check(rnn_states).to(**self.tpdv)
         masks = check(masks).to(**self.tpdv)
